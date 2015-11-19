48e75d5b66ac973ead143ae284ffe185cb54a00f
John Hakala <john_hakala@brown.edu>
2015-11-19 14:15:27 +0100

    add private db file to crab config and update

--- crabConfig_2015DpmptRecoV4_v3.py
+++ crabConfig_2015DpmptRecoV4_v3.py
@@ -1,21 +1,22 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'BHnTuples_2015DpmptRecoV4_oct29'
-config.General.workArea = 'crab_jobs_2015DpmptRecoV4_oct29'
+config.General.requestName = 'BHnTuples_2015DpmptRecoV4_Nov19'
+config.General.workArea = 'crab_jobs_2015DpmptRecoV4_Nov19'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'pmptRecoV4_tuples_2015D_v2.py'
+config.JobType.psetName = 'pmptRecoV4_tuples_2015D_v3.py'
+config.JobType.inputFiles=['Summer15_25nsV6_DATA.db','Summer15_25nsV6_DATA']
 
 config.Data.inputDataset = '/JetHT/Run2015D-PromptReco-v4/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 35
-config.Data.lumiMask='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-258750_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+config.Data.lumiMask='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
-config.Data.publishDataName = 'BHnTuples_2015DpmptRecoV4_oct29'
+config.Data.outputDatasetTag = 'BHnTuples_2015DpmptRecoV4_Nov19'
 
 config.Site.storageSite = 'T3_US_Brown'
