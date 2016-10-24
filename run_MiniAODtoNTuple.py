1f29fb7f2e242ebcd37e450e8cac801111655768
Martin <ka_hei_martin_kwok@brown.edu>
2016-10-25 00:04:29 +0200

    improves batch MC miniAODtoNTuple script

--- run_MiniAODtoNTuple.py
+++ run_MiniAODtoNTuple.py
@@ -7,16 +7,27 @@ folder_list = os.listdir(Topdir)
 print folder_list
 
 
-jobdate="1608"
+jobdate="161007"
+#jobdate="161004"
+#jobdate="160829"
 for dir in folder_list:
+	if (not "charybdis" in dir): 	continue
+	print " Working on this masspoint now: %s" % dir
 	#print "%s%s/*/%s*/*/*.root"%(Topdir,dir,jobdate)
-	flist = glob.glob("%s%s/*/%s*/*/*.root"%(Topdir,dir,jobdate))
-	fname = flist[0]
-	#outname = Outdir+dir+"_NTuple.root"
-	outname = dir+"_NTuple.root"
-	cmd = "cmsRun bh80Xcfg_MC.py InputFile=%s OutputFile=%s"%( fname, outname)
-	if not(os.path.exists(outname)):
-	        print cmd
-	#        os.system(cmd)
+	#flist = glob.glob("%s%s/*/%s*/*/*.root"%(Topdir,dir,jobdate))
+	flist = glob.glob("%s%s/*/*/*/*.root"%(Topdir,dir))
+	if(len(flist)>0):
+		fname = flist[0]
+		#outname = Outdir+dir+"_NTuple.root"
+		if ("AOD" in dir):
+			outname = dir.replace("_AOD","")+"_NTuple.root"
+		else:
+			outname = dir+"_NTuple.root"
+		cmd = "cmsRun bh80Xcfg_MC.py InputFile=%s OutputFile=%s"%( fname, outname)
+		if not(os.path.exists(outname)):
+		        print cmd
+		        os.system(cmd)
+		else:
+		        print "Already produced this masspoint %s...skipping" % dir
 	else:
-	        print "Already produced this masspoint %s...skipping" % dir
+		print "This mass point %s is empty" % (dir) 
